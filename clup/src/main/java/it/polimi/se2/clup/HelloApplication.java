f719bb47263d822d465d9e08bb2ce96a50b67b34
Thomas Bonenfant <tomeinstein18@gmail.com>
2021-01-20 11:48:01 +0100

    added maven project

---
+++ clup/src/main/java/it/polimi/se2/clup/HelloApplication.java
@@ -0,0 +1,8 @@
+package it.polimi.se2.clup;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+}
